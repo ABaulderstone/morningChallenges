e08c8915793c1dbd1f981054885ded8a53f2742a
admin <admin@CA-EDU04.modem>
2021-05-27 09:10:26 +1000

    Added hex program

--- Week-7/hex.js
+++ Week-7/hex.js
@@ -1,14 +1,15 @@
 // Hex to ASCII
-
+// Author: Rhys
 function hexStringToAscii(hexString) {
     // Split input string on spaces 
     const splitOnSpaces = hexString.split(" ")
 
     // Iterate over each character code and map to ascii 
     const mapped = splitOnSpaces.map(charCode => {
-    // We use the static method on the String class fromCharCode on each charCode to convert to ascii value - we need this to be a Number first, it is a string currently
-    // parseInt takes the string and converts it to a number, the second parameter allows us to state what base the number currently is - 16 for hexadecimal
-    // parseInt returns a decimal conversion of the passed hexadecimal number - this is then passed to String.fromCharCode
+    /* We use the static method on the String class fromCharCode on each charCode to convert to ascii value - 
+    we need this to be a Number first, it is a string currently parseInt takes the string and converts it to a number, 
+    the second parameter allows us to state what base the number currently is - 16 for hexadecimal parseInt returns a decimal 
+    conversion of the passed hexadecimal number - this is then passed to String.fromCharCode */
        return String.fromCharCode(parseInt(charCode, 16))
     });
 
