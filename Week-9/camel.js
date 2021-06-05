6b39512bec3e89e7afea81acdef078f313ead38f
admin <admin@CA-EDU04.modem>
2021-06-05 11:00:59 +1000

    Added more comments

--- Week-9/camel.js
+++ Week-9/camel.js
@@ -1,6 +1,8 @@
 function toCamelCase(string){
     // implicit return
     let camel =  string.split(/-|_/).map((element, index) => (
+        /* Must use the | operand if not using [] brackets in regex, 
+        pattern will not be picked up otherwise*/
         index === 0 ? element : element[0].toUpperCase() + element.substring(1)
     )).join("");
 
